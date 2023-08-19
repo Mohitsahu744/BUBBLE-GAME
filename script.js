0c05193bcea5916a65a8190974c2526185208621
Mohitsahu744 <sahumohit1437@gmail.com>
2023-08-19 17:32:31 +0530

    completed

--- script.js
+++ script.js
@@ -1,23 +1,61 @@
-function makeBubble(){
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+var counter;
+
+function makeBubble() {
     var clutter = "";
 
-for(var i=0; i<=183; i++){
-    clutter += `<div class="bubble">${Math.floor(Math.random()*10)}</div>`
-}
-document.querySelector('#pbtm').innerHTML = clutter;
+    for (var i = 0; i <= 183; i++) {
+        clutter += `<div class="bubble">${Math.floor(Math.random() * 10)}</div>`
+    }
+    document.querySelector('#pbtm').innerHTML = clutter;
 }
 
-var timer = 60;
-function runTimer(){
-   var counter = setInterval(function(){
-        if(timer>0){
+function runTimer() {
+    counter = setInterval(function () {
+        if (timer > 0) {
             timer--;
-            document.querySelector('#timerVal').textContent=timer;
+            document.querySelector('#timerVal').textContent = timer;
         }
-        else{
+        else {
             clearInterval(counter);
+            document.querySelector("#hitVal").textContent = 0;
+            document.querySelector('#pbtm').innerHTML = `<h1>Game Over</h1>`
         }
-    },1000);
+    }, 1000);
 }
+
+function hitVal() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitVal").textContent = hitrn;
+}
+
+function increaseScore() {
+    score += 10;
+    document.querySelector('#scoreVal').textContent = score;
+}
+
+function checkHitNum() {
+    document.querySelector('#pbtm')
+        .addEventListener("click", function (dets) {
+            var clicknumber = Number(dets.target.textContent);
+            if (clicknumber == hitrn) {
+                increaseScore();
+                hitVal();
+                makeBubble();
+            }
+            else if (clicknumber !== hitrn) {
+                // console.log(clicknumber)
+                clearInterval(counter);
+                document.querySelector('#pbtm').innerHTML = `<h1>Wrong Value hit</h1>`
+            }
+
+        })
+}
+
 makeBubble();
-runTimer()
+runTimer();
+hitVal();
+checkHitNum();
+
